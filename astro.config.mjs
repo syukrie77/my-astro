bea3ec05e78ce1d73d28dc93cb6a569feb6191da
Muhammad Syukri <syukrie77@gmail.com>
2024-09-16 12:45:20 +0700

    add admin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
+{/* import CMS from "decap-cms-app";
+// Initialize the CMS object
+CMS.init();
+// Now the registry is available via the CMS object.
+CMS.registerPreviewTemplate("my-template", MyTemplate); */}
 
 // https://astro.build/config
 export default defineConfig({
